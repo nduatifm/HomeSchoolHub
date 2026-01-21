879d09e98718af33c4bd00e828c03188863510df
inydv <im.nydv@gmail.com>
2026-01-21 13:57:13 +0530

    fix package and vite

--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,7 @@ export default defineConfig({
   root: path.resolve(import.meta.dirname, "client"),
   build: {
     outDir: path.resolve(import.meta.dirname, "dist/public"),
+    chunkSizeWarningLimit: 1500,
     emptyOutDir: true,
   },
 });
